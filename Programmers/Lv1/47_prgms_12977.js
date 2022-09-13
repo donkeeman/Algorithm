af40bc0771b31e9733548b9210311447f374464b
donkeeman <doonkeeemaan@gmail.com>
2022-09-13 17:02:27 +0900

    프로그래머스 소수 만들기 풀이

---
+++ Programmers/Lv1/47_prgms_12977.js
@@ -0,0 +1,27 @@
+function isPrime(n){
+    if(n < 2){
+        return false;
+    }
+    for(let i = 2; i<Math.sqrt(n)+1; i++){
+        if(n % i === 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+function solution(nums) {
+    let answer = 0;
+    
+    for(let i = 0; i < nums.length - 2; i++){
+        for(let j = i+1; j < nums.length - 1; j++){
+            for(let k = j+1; k < nums.length; k++){
+                if(isPrime(nums[i] + nums[j] + nums[k])){
+                    answer++;
+                }
+            }
+        }
+    }
+    
+    return answer;
+}
