e2748be593fbbb9a3bb18ed14038ecc9b765efb6
donkeeman <doonkeeemaan@gmail.com>
2022-11-07 02:11:22 +0900

    프로그래머스 숨어있는 숫자의 덧셈 (1) 풀이

--- Programmers/Lv0/036_prgrms_120851.js
+++ Programmers/Lv0/036_prgrms_120851.js
@@ -1,3 +1,3 @@
 function solution(my_string) {
-    return my_string.match(/[0-9]/g).reduce((a, b) => parseInt(a) + parseInt(b), 0);
+    return my_string.match(/[0-9]/g).reduce((a, b) => a + parseInt(b), 0);
 }
