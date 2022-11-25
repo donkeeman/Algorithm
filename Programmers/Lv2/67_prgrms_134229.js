c87481b7cc7227b767bd2e3942a2b281fcc3b8c1
donkeeman <doonkeeemaan@gmail.com>
2022-11-25 17:54:48 +0900

    프로그래머스 우박수열 정적분 풀이

---
+++ Programmers/Lv2/67_prgrms_134229.js
@@ -0,0 +1,21 @@
+function solution(k, ranges) {
+    const answer = [];
+    const collatz = [];
+    while(k !== 1){
+        let n = k;
+        if(!(n % 2)){
+            n /= 2;
+        }
+        else{
+            n = n * 3 + 1;
+        }
+        collatz.push((k + n) / 2);
+        k = n;
+    }
+    ranges.map(([start, offset]) => {
+        answer.push(
+            collatz.length + offset === start ? 0.0 
+            : collatz.length + offset < start ? -1.0 
+            : collatz.slice(start, collatz.length + offset).reduce((a, b) => a + b, 0))});
+    return answer;
+}
