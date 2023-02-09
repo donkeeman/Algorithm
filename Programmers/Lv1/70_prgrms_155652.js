f9aafa5da1dbb522f271a3773695529d088a1889
donkeeman <doonkeeemaan@gmail.com>
2023-02-09 16:58:17 +0900

    프로그래머스 둘만의 암호 풀이 다른 방법 추가

--- Programmers/Lv1/70_prgrms_155652.js
+++ Programmers/Lv1/70_prgrms_155652.js
@@ -1,18 +1,27 @@
+// 수정한 풀이
 function solution(s, skip, index) {
-    skip = skip.split("").map(n => n.charCodeAt());
-    s = s.split("").map(c => {
-        const alphabet = [];
-        c = (c.charCodeAt() + 1 - 97) % 26 + 97;
-        while(true){
-            if(!skip.includes(alphabet[index - 1]) && alphabet.length === index){
-                break;
-            }
-            if(!skip.includes(c)){
-                alphabet.push(c);
-            }
-            c = (c - 96) % 26 + 97;
-        }
-        return String.fromCharCode(alphabet[index - 1]);
-    });
+    const alphabet = "abcdefghijklmnopqrstuvwxyz".match(new RegExp(`[^${skip}]`, "g"));
+    const length = alphabet.length;
+    s = s.split("").map(c => alphabet[(alphabet.indexOf(c) + index) % length]);
     return s.join("");
 }
+
+// 처음에 풀었던 풀이
+// function solution(s, skip, index) {
+//     skip = skip.split("").map(n => n.charCodeAt());
+//     s = s.split("").map(c => {
+//         const alphabet = [];
+//         c = (c.charCodeAt() + 1 - 97) % 26 + 97;
+//         while(true){
+//             if(!skip.includes(alphabet[index - 1]) && alphabet.length === index){
+//                 break;
+//             }
+//             if(!skip.includes(c)){
+//                 alphabet.push(c);
+//             }
+//             c = (c - 96) % 26 + 97;
+//         }
+//         return String.fromCharCode(alphabet[index - 1]);
+//     });
+//     return s.join("");
+// }
